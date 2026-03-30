46c82003a30b2a806817455d9d60058e439601e2
andsovko007 <andsovko@gmail.com>
2026-03-30 11:29:54 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AboutPage from './AboutPage.jsx'
+
+function RootRouter() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+
+  if (path === '/about') {
+    return <AboutPage />
+  }
+
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RootRouter />
   </React.StrictMode>,
 )
